5bef6ace4c0d16a5a8ff775c8f3ff45d40a2ca9d
KAYSILVHA <114036212+KAYSILVHA@users.noreply.github.com>
2026-01-07 19:39:33 +0000

    adicionando teste de roteamento

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { View, Text, Button } from "react-native";
+import { router } from "expo-router";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Bem-vindo à Tela Inicial!</Text>
+      <Button
+        title="Ir para a Tela 2"
+        onPress={() => router.push("/src/screens/description")}
+      />
     </View>
   );
 }
---
+++ app/src/screens/description.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function DescriptionScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Bem-vindo à Tela 2!</Text>
+    </View>
+  );
+}
